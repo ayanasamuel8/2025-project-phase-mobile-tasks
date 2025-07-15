cc0b170fe3d6319fd82045635bbc635cb7fa4ab9
Ayana Samuel <ayanasamuel8@gmailcom>
2025-07-15 10:58:04 +0300

    init: initialized repo with product model

---
+++ on-boarding/product-2/lib/src/models/product.dart
@@ -0,0 +1,55 @@
+class Product {
+  final String _name;
+  final double _price;
+  final String _description;
+  static int _idCounter = 0;
+  final int _id;
+
+  Product({required String name, required double price, String? description})
+    : _id = _idCounter++,
+      _name = name,
+      _price = price,
+      _description = description ?? '';
+
+  Product._withId({
+    required int id,
+    required String name,
+    required double price,
+    String? description,
+  }) : _id = id,
+       _name = name,
+       _price = price,
+       _description = description ?? '' {
+    if (id >= _idCounter) {
+      _idCounter = id + 1;
+    }
+  }
+
+  String get name => _name;
+  double get price => _price;
+  String get description => _description;
+  int get id => _id;
+
+  @override
+  String toString() {
+    return 'Product{id: $_id, name: $_name, price: $_price, description: $_description}';
+  }
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product._withId(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      price: (json['price'] as num).toDouble(),
+      description: json['description'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': _id,
+      'name': _name,
+      'price': _price,
+      'description': _description,
+    };
+  }
+}
