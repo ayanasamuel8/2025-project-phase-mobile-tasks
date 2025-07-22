58f9aa98c5155bba52187da40f826011b621396f
Ayana Samuel <ayanasamuel8@gmailcom>
2025-07-22 13:09:39 +0300

    update: unesessary import removed

--- on-boarding/basic_ecommerce/android/app/build.gradle.kts
+++ on-boarding/basic_ecommerce/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.basic_ecommerce"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = 27.0.12077973
+    ndkVersion = "27.0.12077973"  // Note the quotes around the version
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- on-boarding/basic_ecommerce/lib/views/widgets/text_field.dart
+++ on-boarding/basic_ecommerce/lib/views/widgets/text_field.dart
@@ -1,4 +1,3 @@
-import 'package:basic_ecommerce/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
