1da0bcc92658e960e7113cb09ca2fe3a7c8ff5e2
Ayana Samuel <ayanasamuel8@gmailcom>
2025-07-24 09:39:05 +0300

    update: removed the basic_ecommerce directory

--- on-boarding/basic_ecommerce/lib/constants/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CustomColor {
-  static const grey = Color.fromARGB(255, 170, 170, 170);
-  static const darkGrey = Color.fromARGB(255, 102, 102, 102);
-  static const lightGrey = Color.fromARGB(255, 239, 239, 239);
-  static const black = Colors.black;
-}
